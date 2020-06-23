e56f10128c00f60c5009128c2e3122b532a4f9da
Alex Sinclair <amansinclair@gmail.com>
2020-06-23 09:31:08 +0200

    initial cartpole tests

--- RL/solvers/policy_grad.py
+++ RL/solvers/policy_grad.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
 import gym
 import time
@@ -15,48 +16,72 @@ class Net(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.softmax(x)
-
-
-learning_rate = 0.01
+        return F.softmax(x, dim=0)
 
 
 def get_return(rewards):
-    return rewards[::-1].cumsum()
+    g = torch.flip(rewards, [0]).cumsum(0)
+    g = torch.flip(g, [0])
+    return g
 
 
-def play(net, env):
-    states = []
-    actions = []
-    rewards = []
+def play_single(net, env, view=False):
+    probs = torch.zeros((200), dtype=torch.float32)
+    rewards = torch.zeros((200), dtype=torch.float32)
     observation = torch.tensor(env.reset(), dtype=torch.float32)
-    states.append(observation)
     done = False
     i = 0
-    while i < 1000 and not done:
-        env.render()
-        time.sleep(0.1)
-        i += 1
-        probs = net(observation)
-        print(probs)
-        # probs = net(observation)
-        action = np.random.choice([0, 1], p=probs.detach().numpy())
-        # action = np.random.choice([0, 1])
-        actions.append(action)
+    while not done:
+        if view:
+            env.render()
+            time.sleep(0.1)
+        p = net(observation)
+        action = np.random.choice([0, 1], p=p.detach().numpy())
+        probs[i] = p[action]
         observation, reward, done, info = env.step(action)
         observation = torch.tensor(observation, dtype=torch.float32)
-        rewards.append(reward)
-        states.append(observation)
-    r = get_return(np.array(rewards, dtype="float32"))
-    print(r)
-    return (
-        torch.stack(states),
-        torch.tensor(actions, dtype=torch.double),
-        torch.flip(torch.from_numpy(r)),
-    )
+        rewards[i] = reward
+        i += 1
+    g = get_return(rewards[:i])
+    return (probs[:i], g)
 
 
+def play(net, env, batch_size=1000):
+    probs = []
+    gs = []
+    current_size = 0
+    i = 0
+    while current_size < batch_size:
+        prob, g = play_single(net, env)
+        probs.append(prob)
+        gs.append(g)
+        current_size += prob.shape[0]
+        i += 1
+    return i, torch.cat(probs), torch.cat(gs)
+
+
+def train_batch(net, opt, env):
+    opt.zero_grad()
+    i, probs, gs = play(net, env)
+    loss = -(gs * torch.log(probs)).mean()
+    # print(loss)
+    loss.backward()
+    opt.step()
+    return i
+
+
+def view_round(net, env):
+    with torch.no_grad():
+        play_single(net, env, True)
+
+
+learning_rate = 0.01
 net = Net()
+opt = optim.Adam(net.parameters(), lr=learning_rate)
 env = gym.make("CartPole-v1")
-s, a, r = play(net, env)
+n = 0
+for i in range(30):
+    n += train_batch(net, opt, env)
+    print(n)
+view_round(net, env)
 env.close()
