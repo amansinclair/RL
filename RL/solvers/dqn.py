27430d7b0b204862dce2d03c34b9c6752eec5232
Alex Sinclair <amansinclair@gmail.com>
2020-06-29 13:21:30 +0200

    gamma added to loss

--- RL/solvers/dqn.py
+++ RL/solvers/dqn.py
@@ -20,10 +20,10 @@ class Q(nn.Module):
 
 
 class DQN:
-    def __init__(self, env, batch_size=10, lr=0.01, gamma=1.0, e=0.1):
+    def __init__(self, env, batch_size=100, lr=0.01, gamma=1.0, e=0.01):
         n_inputs = n_inputs = env.observation_space.shape[0]
         n_outputs = env.action_space.n
-        self.replays = deque(maxlen=100)
+        self.replays = deque(maxlen=1000)
         self.Q = Q(n_inputs, n_outputs)
         self.opt = optim.Adam(self.Q.parameters(), lr=lr)
         self.batch_size = batch_size
@@ -40,7 +40,7 @@ class DQN:
             self.replays.append((previous_obs, previous_a, reward, observation))
         with torch.no_grad():
             action_values = self.Q(observation)
-        max_action = torch.argmax(action_values)
+        max_action = torch.argmin(action_values)
         probs = self.base_probs.copy()
         probs[max_action] = self.max_prob
         action = random.choices(self.actions, weights=probs)[0]
@@ -69,7 +69,7 @@ class DQN:
                 q = 0
                 s, a, r, s2 = replay
                 if s2 != None:
-                    q = torch.max(self.Q(s2))
+                    q = self.gamma * torch.max(self.Q(s2))
                 labels[i] = r + q
         return labels
 
@@ -79,5 +79,5 @@ class DQN:
         return self.Q(states)[:, actions]
 
     def calculate_loss(self, labels, qs):
-        return ((labels - qs) ** 2).mean()
+        return ((qs - labels) ** 2).mean()
 
--- solutions/cartpole_solve_dqn.py
+++ solutions/cartpole_solve_dqn.py
@@ -4,18 +4,23 @@ from RL.solvers import DQN
 import numpy as np
 
 env = gym.make("CartPole-v1")
-lr = 0.01
+env._max_episode_steps = 1000
+lr = 0.001
 
-agent = DQN(env, lr=lr, gamma=0.99)
+agent = DQN(env, lr=lr, gamma=0.99, e=0.1)
 total_rewards = []
-n_episodes = 1
+n_episodes = 500
+actions = []
 for episode in range(n_episodes):
     observation = env.reset()
     done = False
     reward = None
     rewards = []
+    last_actions = []
     while not done:
         action = agent.step(observation, reward)
+        actions.append(action)
+        last_actions.append(action)
         observation, reward, done, info = env.step(action)
         rewards.append(reward)
     agent.update(observation, reward)
@@ -25,3 +30,5 @@ for episode in range(n_episodes):
 env.close()
 plt.plot(total_rewards)
 plt.show()
+print(last_actions)
+print(sum(actions) / len(actions))
