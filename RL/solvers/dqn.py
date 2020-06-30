830fdc26df24de2835c6458ad8939e9a4183d417
Alex Sinclair <amansinclair@gmail.com>
2020-06-30 19:47:53 +0200

    dqn still not working

--- RL/solvers/dqn.py
+++ RL/solvers/dqn.py
@@ -3,81 +3,128 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-from collections import deque
+from collections import deque, namedtuple
 import random
 
 
 class Q(nn.Module):
-    def __init__(self, n_inputs, n_outputs, size=32):
+    def __init__(self, n_inputs, n_outputs, size=64):
         super().__init__()
         self.fc1 = nn.Linear(n_inputs, size)
-        self.fc2 = nn.Linear(size, n_outputs)
+        self.fc2 = nn.Linear(size, size)
+        self.fc3 = nn.Linear(size, n_outputs)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
+Replay = namedtuple("Replay", "state action reward next_state is_done")
+
+
+class Replays:
+    def __init__(self, maxlen=1000):
+        self.maxlen = maxlen
+        self.count = 0
+        self.replays = deque(maxlen=maxlen)
+
+    def add(self, replay):
+        self.replays.append(replay)
+        self.count += 1
+
+    def __len__(self):
+        return min(self.count, self.maxlen)
+
+    def get_batch(self, size):
+        size = min(size, len(self))
+        batch = random.sample(self.replays, size)
+        states = []
+        actions = []
+        rewards = []
+        next_states = []
+        is_dones = []
+        for state, action, reward, next_state, is_done in batch:
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            next_states.append(next_state)
+            is_dones.append(float(not (is_done)))
+        return (
+            torch.stack(states),
+            torch.tensor(actions).view(-1, 1),
+            torch.tensor(rewards),
+            torch.stack(next_states),
+            torch.tensor(is_dones),
+        )
+
+
 class DQN:
-    def __init__(self, env, batch_size=100, lr=0.01, gamma=1.0, e=0.01):
+    def __init__(
+        self, env, batch_size=128, lr=0.01, gamma=0.99, e=0.1, steps_per_update=10
+    ):
         n_inputs = n_inputs = env.observation_space.shape[0]
         n_outputs = env.action_space.n
-        self.replays = deque(maxlen=1000)
+        self.replays = Replays()
         self.Q = Q(n_inputs, n_outputs)
         self.opt = optim.Adam(self.Q.parameters(), lr=lr)
         self.batch_size = batch_size
         self.gamma = gamma
-        self.base_probs = [e / n_outputs] * n_outputs
+        self.steps_per_update = steps_per_update
+        self.base_probs = [e / (n_outputs - 1)] * n_outputs
         self.max_prob = 1 - e
         self.actions = [a for a in range(n_outputs)]
         self.previous = None
+        self.steps = 0
+        self.loss = nn.MSELoss()
 
-    def step(self, observation, reward=None):
+    def step(self, observation, reward=None, is_done=False):
+        self.steps += 1
         observation = torch.tensor(observation, dtype=torch.float32)
+        action = self.get_action(observation)
         if reward != None:
             previous_obs, previous_a = self.previous
-            self.replays.append((previous_obs, previous_a, reward, observation))
-        with torch.no_grad():
-            action_values = self.Q(observation)
-        max_action = torch.argmin(action_values)
-        probs = self.base_probs.copy()
-        probs[max_action] = self.max_prob
-        action = random.choices(self.actions, weights=probs)[0]
+            self.replays.add(
+                Replay(previous_obs, previous_a, reward, observation, is_done)
+            )
+            if (
+                len(self.replays) >= self.batch_size
+                and self.steps % self.steps_per_update == 0
+            ):
+                self.update()
         self.previous = (observation, action)
         return action
 
-    def update(self, observation, reward):
-        previous_obs, previous_a = self.previous
-        self.replays.append((previous_obs, previous_a, reward, None))
-        batch_size = min(self.batch_size, len(self.replays))
-        replays = random.choices(self.replays, k=batch_size)
-        self.update_weights(replays)
+    def get_action(self, observation):
+        with torch.no_grad():
+            action_values = self.Q(observation)
+        max_action = torch.argmax(action_values)
+        probs = self.base_probs.copy()
+        probs[max_action] = self.max_prob
+        action = random.choices(self.actions, weights=probs)[0]
+        return action
 
-    def update_weights(self, replays):
+    def update(self):
+        states, actions, rewards, next_states, is_dones = self.replays.get_batch(
+            self.batch_size
+        )
         self.opt.zero_grad()
-        labels = self.get_labels(replays)
-        qs = self.get_qs(replays)
-        loss = self.calculate_loss(labels, qs)
+        ys = self.get_labels(rewards, next_states, is_dones)
+        qs = self.get_qs(states, actions)
+        loss = self.loss(qs, ys)  # calculate_loss(qs, ys)
         loss.backward()
         self.opt.step()
 
-    def get_labels(self, replays):
+    def get_labels(self, rewards, next_states, is_dones):
         with torch.no_grad():
-            labels = torch.zeros(len(replays))
-            for i, replay in enumerate(replays):
-                q = 0
-                s, a, r, s2 = replay
-                if s2 != None:
-                    q = self.gamma * torch.max(self.Q(s2))
-                labels[i] = r + q
-        return labels
+            qmax, idxs = torch.max(self.Q(next_states), dim=1)
+            y = (self.gamma * qmax * is_dones) + rewards
+        return y
 
-    def get_qs(self, replays):
-        states = torch.stack([s for s, a, r, s2 in replays])
-        actions = [a for s, a, r, s2 in replays]
-        return self.Q(states)[:, actions]
+    def get_qs(self, states, actions):
+        return torch.gather(self.Q(states), 1, actions).view(-1)
 
-    def calculate_loss(self, labels, qs):
-        return ((qs - labels) ** 2).mean()
+    # def calculate_loss(self, qs, ys):
+    #    return ((qs - ys) ** 2).mean()
 
--- solutions/cartpole_solve_dqn.py
+++ solutions/cartpole_solve_dqn.py
@@ -2,14 +2,15 @@ import gym
 import matplotlib.pyplot as plt
 from RL.solvers import DQN
 import numpy as np
+import torch
 
 env = gym.make("CartPole-v1")
 env._max_episode_steps = 1000
 lr = 0.001
 
-agent = DQN(env, lr=lr, gamma=0.99, e=0.1)
+agent = DQN(env, lr=lr, gamma=0.99, e=0.05)
 total_rewards = []
-n_episodes = 500
+n_episodes = 200
 actions = []
 for episode in range(n_episodes):
     observation = env.reset()
@@ -18,15 +19,16 @@ for episode in range(n_episodes):
     rewards = []
     last_actions = []
     while not done:
-        action = agent.step(observation, reward)
+        action = agent.step(observation, reward, done)
         actions.append(action)
         last_actions.append(action)
         observation, reward, done, info = env.step(action)
         rewards.append(reward)
-    agent.update(observation, reward)
+    agent.step(observation, reward, done)
     total_reward = sum(rewards)
     total_rewards.append(total_reward)
     print(episode + 1, total_reward)
+print("FINAL Q", agent.Q(torch.tensor(observation, dtype=torch.float32)))
 env.close()
 plt.plot(total_rewards)
 plt.show()
